c2e66b8d6f92fd1ecc2f7fa9a55c9fb09a1524ac
karthik-ivs <ivskarthik28may2006@gmail.com>
2026-06-15 10:51:35 +0530

    Exercise_1

--- upSkilling/M3/Exercise01_SetupEnvironment/Program.cs
+++ upSkilling/M3/Exercise01_SetupEnvironment/Program.cs
@@ -1,5 +1,9 @@
-// Exercise01 - Hello World Setup
-// A minimal console app proves that the .NET SDK and project setup work.
+using System;
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("The .NET console application is running successfully.");
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World");
+    }
+}
