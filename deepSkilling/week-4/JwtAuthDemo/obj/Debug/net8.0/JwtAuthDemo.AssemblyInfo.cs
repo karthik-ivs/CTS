f898bb7e142a13eeb6d605c8a3a133c4926bbc9f
karthik-ivs <ivskarthik28may2006@gmail.com>
2026-07-21 18:48:57 +0530

    Modified Week 4

--- deepSkilling/week-4/JwtAuthDemo/obj/Debug/net8.0/JwtAuthDemo.AssemblyInfo.cs
+++ deepSkilling/week-4/JwtAuthDemo/obj/Debug/net8.0/JwtAuthDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JwtAuthDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dea1984bafedf6ff353980046a25d938f16fe875")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96df5dd6f3d686ee0b84c9815e7379c0d8372134")]
 [assembly: System.Reflection.AssemblyProductAttribute("JwtAuthDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JwtAuthDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
