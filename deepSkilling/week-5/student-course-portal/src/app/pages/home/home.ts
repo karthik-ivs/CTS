adbf63ca2036931769e168dc6d85a011e8e619d2
karthik-ivs <ivskarthik28may2006@gmail.com>
2026-07-23 09:09:27 +0530

    Hands on 2

--- deepSkilling/week-5/student-course-portal/src/app/pages/home/home.ts
+++ deepSkilling/week-5/student-course-portal/src/app/pages/home/home.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home {}
+export class Home {
+
+  // Interpolation
+  portalName = 'Student Course Portal';
+
+  // Property Binding
+  isPortalActive = true;
+
+  // Event Binding
+  message = '';
+
+  // Two-way Binding
+  searchTerm = '';
+
+  // Called when the Enroll Now button is clicked
+  onEnrollClick(): void {
+    this.message = 'Enrollment opened!';
+  }
+}
